c59b00a455479d5d8abfc324762231b358c5fc6b
Henrik Bonsmann <112704394+Henrik-Bonsmann@users.noreply.github.com>
2022-12-07 13:28:24 +0100

    Update streamlit_git.py
    Comments and selection refinement

--- streamlit_git.py
+++ streamlit_git.py
@@ -8,47 +8,54 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
+# Needed to display the different pages
 placeholder=st.empty()
+
+## Initialize session state variables for later use
 if "page" not in st.session_state:
+    # manages the currently displayed pate
     st.session_state.page = 0
 if "question" not in st.session_state:
+    # manages the currently displayed user query
     st.session_state.question = 0
-
 if "personal" not in st.session_state:
+    # saves user inputs
     st.session_state.personal = pd.DataFrame(columns =  ['symptom_desc', 'class_desc'])
-
 if "patient_data" not in st.session_state:
+    # holds translated model inputs
     st.session_state.patient_data = pd.DataFrame(columns =  ['disease', 'input']) 
 
-def diagnose(): 
+def diagnose():
+    # After the initial choice, extract the relevant diseases 
     st.session_state.diseases = set(symptoms[symptoms['chosen']]['diseases'].sum())
     st.session_state.page = 1
 
 def reset():
+    # reset session state variables to initial values. Start anew.
     st.session_state.page = 0
     st.session_state.question = 0
     st.session_state.personal = pd.DataFrame(columns =  ['symptom_desc', 'class_desc'])
     st.session_state.patient_data = pd.DataFrame(columns =  ['disease', 'input']) 
 
 def results():
+    # display the results page on button click
     st.session_state.page = 2
 
 def next_q(answer, q):
+    # After input, save the signifier/value pair to a dataframe and show the next query.
     st.session_state.question +=1
     new  = pd.DataFrame([[q, answer]], columns = ['symptom_desc', 'class_desc'])
     st.session_state.personal = pd.concat([st.session_state.personal, new], axis = 'index', ignore_index=True)
 
 def mk_model_in(dis, df):
+    # takes a constructed dataframe of model inputs and the associated disease and saves them for further use
     if ~(dis in st.session_state.patient_data['disease']):
         new = pd.DataFrame([[dis, dict(zip(df['symptom'], df['value']))]], columns = ['disease', 'input'])
         st.session_state.patient_data = pd.concat([st.session_state.patient_data, new], axis = 'index', ignore_index=True)
     
 
+# General symptoms, their group and associated diseases. Used to determine which inputs to query.
 symptoms = pd.DataFrame(data=np.array([
-    [False, 'Education', 'Personal Background', ['Alzheimer', 'Stroke']],
-    [False,'Wealth', 'Personal Background', ['Alzheimer', 'Stroke']],
-    [False,'Smoking', 'Personal Background', ['Lung_Cancer','Stroke']],
-    [False,'Alcohol', 'Personal Background', ['Lung_Cancer']],
     [False,'Anxiety', 'Mental/Cognitive', ['Lung_Cancer']],
     [False,'Appetite Loss', 'Mental/Cognitive', ['Kidney_Disease']],
     [False,'Peer Pressure', 'Mental/Cognitive', ['Lung_Cancer']],
@@ -71,6 +78,7 @@ symptoms = pd.DataFrame(data=np.array([
     ], dtype = object), columns = ['chosen', 'name', 'group', 'diseases'])
 
 
+# First page. Display groups of general symptoms.
 if st.session_state.page == 0:
     with placeholder.container():
         for group in symptoms["group"].unique():   
@@ -79,52 +87,78 @@ if st.session_state.page == 0:
                     symptoms.loc[symptoms["name"] == symp, 'chosen'] = st.checkbox(symp, False)
         st.button('Diagnosis',on_click=diagnose, type='primary', disabled = ~symptoms['chosen'].any())
 
+# Second page, query user inputs
 elif st.session_state.page == 1:
     with placeholder.container():
-        ds = pd.read_csv(str(Path.cwd())+r'/disease_symptoms.csv')
+        # dataframe detailing the relationship between symptoms, diseases, signifiers and human->machine translation
+        ds = pd.read_csv(str(Path.cwd())+r'\datasets\disease_symptoms.csv')
 
+        #st.write(ds[ds['disease'].isin(st.session_state.diseases)])
+
+        # Query input for every symptom needed.
         for idx, q in enumerate(ds[ds['disease'].isin(st.session_state.diseases)]['symptom_desc'].unique()):
+            # displays only one question at a time
             if st.session_state.question == idx:
                 st.write(f"Please provide additional information on patient's {q}:")
+
+                # All symptoms that will be updated from this query
                 symps = ds[ds['symptom_desc']==q]
+                # A value needs to be continuous when even one of them is not categorical
+                # Categorical values will a list structure in "class" and thus contain ','
                 is_cont = ~symps['class'].str.contains(',').all()
                 
                 if(is_cont):
+                    # Continuous variables list a min-max range in 'class'. 
+                    # Combine all of them and take the ultimate min and max from there.
                     thresholds = list(map(float,(symps[(~ds['class'].str.contains(','))]["class"].str.split(' - ').values.sum())))
                     maximum = max(thresholds)
                     minimum = min(thresholds)
                     label = f"{minimum} - {maximum}"
+
+                    # take continous value as free text-input
                     answer = st.text_input(label = label, key = q)
                             
                 elif(~is_cont):
+                    # Categorical values that are not humanly legible include a dictionary of translations in 'class_desc'
                     translation = ~symps['class_desc'].isna().all()
 
                     if(translation):
                         options = list(ast.literal_eval(symps[~symps['class_desc'].isna()]['class_desc'].iloc[0]).keys())
                     else:
+                        # Take the option with the most categories to display. 
+                        # Other options will have a dictionary to translate to the lower number of options
                         here = symps['class'].str.len().nlargest(1).index
                         options = ast.literal_eval(ds['class'].iloc[here].values[0])
 
                     answer = st.selectbox(label='', options = options, key = q, label_visibility = 'collapsed')
-                             
+
+                # Confirm input and get the next question             
                 st.button("Next", on_click=next_q, args=[answer, q])               
         
+        # Once all the inputs have been made
         if st.session_state.question == len(ds[ds['disease'].isin(st.session_state.diseases)]['symptom_desc'].unique()):
+            # display input overview
             st.write(st.session_state.personal)
+            # Extract specific inputs for each model
             for dis in st.session_state.diseases:
+                # Get only input relevant to the disease
                 this = st.session_state.personal.merge(ds[ds['disease']==dis], on='symptom_desc', how='inner')
+                
+                # If no translation is necessary, take the input directly
                 this.loc[this['class_desc_y'].isna(),'value']= this['class_desc_x']
+        
                 for ii in this[this['bnr'].isna() & ~this['class_desc_y'].isna()].index:
+                    # If a translation dictionary is present, translate the human input into correct model label
                     this.iloc[ii, this.columns.get_indexer(['value'])] = eval(this.iloc[ii]['class_desc_y'])[this.iloc[ii]['class_desc_x']]
                 for ii in this[~this['bnr'].isna()].index:
+                    # If a binary classification from a continuous variable is needed, check against threshold and translate into correct model label
                     this.iloc[ii, this.columns.get_indexer(['value'])] = eval(this.iloc[ii]['class_desc_y'])[float(this.iloc[ii]['class_desc_x']) > float(this.iloc[ii]['bnr'])]
 
-                #st.write( dict(zip(this['symptom'], this['value'])))    
+                # Save the extracted values to use in  
                 mk_model_in(dis, this)
-                #st.text(st.session_state.patient_data)
             
-            st.button('Show Results',on_click=results, type='primary')   
-        #st.write(answer)       
+            # Go to next page to show results
+            st.button('Show Results',on_click=results, type='primary')             
         
         
 
